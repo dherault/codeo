2b71a0d81b8d5bd5f5b99597decaaeeba96355d2
David Hérault <dherault@gmail.com>
2022-03-25 09:05:12 +0200

    Ehance solids

--- front/src/components/Solids.js
+++ front/src/components/Solids.js
@@ -25,8 +25,11 @@ function runCanvas(element) {
   let renderer
   let controls
   let directionalLight
+  let raycaster
+  let gridHelper
   let stopped = false
 
+  const mouse = new THREE.Vector2()
   const state = {
     isSelectingFaces: false,
   }
@@ -45,10 +48,10 @@ function runCanvas(element) {
 
     const light = new THREE.AmbientLight(0x404040) // soft white light
     scene.add(light)
-    directionalLight = new THREE.DirectionalLight(0xffffff, 0.75)
+    directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
     scene.add(directionalLight)
 
-    const gridHelper = new THREE.GridHelper(16, 16, '#bbb', '#bbb')
+    gridHelper = new THREE.GridHelper(16, 16, '#bbb', '#bbb')
     scene.add(gridHelper)
 
     renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
@@ -60,7 +63,13 @@ function runCanvas(element) {
 
     controls = new OrbitControls(camera, renderer.domElement)
 
+    raycaster = new THREE.Raycaster()
+
+    const axesHelper = new THREE.AxesHelper(5)
+    scene.add(axesHelper)
+
     window.addEventListener('resize', onWindowResize)
+    // element.addEventListener('click', onClick, false)
 
     loadMesh('/solids/tetrahedron.obj', { x: 0, y: 0, z: 0 })
     loadMesh('/solids/elongated square cupola.obj', { x: 4, y: 0, z: 0 })
@@ -90,6 +99,39 @@ function runCanvas(element) {
     const gui = new GUI()
     gui.add(state, 'isSelectingFaces').onChange(value => state.isSelectingFaces = value)
   }
+
+  // function onClick(event) {
+  //   if (!state.isSelectingFaces) return
+
+  //   event.preventDefault()
+
+  //   const { width, height, top, left } = element.getBoundingClientRect()
+
+  //   mouse.x = (event.clientX - left) / width * 2 - 1
+  //   mouse.y = -(event.clientY - top) / height * 2 + 1
+
+  //   raycaster.setFromCamera(mouse, camera)
+
+  //   const intersects = raycaster.intersectObjects(scene.children)
+
+  //   intersects.forEach(intersection => {
+  //     const { object, face } = intersection
+
+  //     if (object.type !== 'Mesh') return
+
+  //     object.up = face.normal
+  //     object.lookAt(object.up)
+  //     // object.rotation.set(0, 0, 0)
+
+  //     console.log('object.rotation', object.rotation)
+  //   })
+  //   // if (intersects.length > 0) {
+  //   //   const intersection = intersects[0]
+
+  //   //   // object.geometry.faces[faceIndex].color.set(0xff0000)
+  //   //   // object.geometry.colorsNeedUpdate = true
+  //   // }
+  // }
 
   function animate() {
     if (stopped) return
